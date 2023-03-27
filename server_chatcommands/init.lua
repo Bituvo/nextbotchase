04e8f0e7c8179fae2648de3621f6ad97296512ae
Bituvo <bituvocoding@gmail.com>
2023-03-27 02:40:22 -0400

    Refactor: Move server chat commands to new mod

---
+++ server_chatcommands/init.lua
@@ -0,0 +1,41 @@
+minetest.unregister_chatcommand("serverinfo")
+minetest.unregister_chatcommand("spawn")
+
+minetest.register_chatcommand("spawn", {
+    description = "Teleport yourself or another player to spawn",
+    params = "[player]",
+    func = function(name, param)
+        if param ~= "" then
+            local player = minetest.get_player_by_name(param)
+            
+            if player then
+                if minetest.check_player_privs(name, {server = true}) then
+                    player:set_pos(static_spawn)
+                    minetest.chat_send_player(name, 'Teleported "' .. param .. '" to spawn')
+                else
+                    minetest.chat_send_player(name, "You cannot send another player to spawn")
+                end
+            else
+                minetest.chat_send_player(name, '"' .. param .. '" either does not exist or is not logged in')
+            end
+        else
+            local player = minetest.get_player_by_name(name)
+
+            player:set_pos(static_spawn)
+            minetest.chat_send_player(name, "Teleported to spawn")
+        end
+    end
+})
+
+minetest.register_chatcommand("who", {
+    description = "List who is currently logged in",
+    func = function(name)
+        local message = "Clients: "
+
+        for _, player in ipairs(minetest.get_connected_players()) do
+            message = message .. player:get_player_name() .. ", "
+        end
+
+        minetest.chat_send_player(name, string.sub(message, 1, -3))
+    end
+})
