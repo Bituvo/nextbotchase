c60ab0bae99bc7327b770d16c03936850b05dfec
Bituvo <bituvocoding@gmail.com>
2023-04-14 21:02:11 -0400

    Fix: Say "No reason specified" for empty restart reasons

--- server/chatcommands/init.lua
+++ server/chatcommands/init.lua
@@ -45,6 +45,10 @@ minetest.register_chatcommand("restart", {
     privs = {server = true},
     params = "[reason]",
     func = function(name, param)
+		if param == "" then
+			param = "No reason specified"
+		end
+		
         minetest.chat_send_all(minetest.colorize("red", "Server restart requested by " .. name .. ": " .. param .. " (wait a bit before reconnecting)"))
         minetest.request_shutdown(param .. "\n\nWait a bit before reconnecting.\nIf the server doesn't reboot, Discord PM the admin: Thresher#9632", true, 20)
     end
