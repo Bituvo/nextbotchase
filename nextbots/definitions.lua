ca3f474eda18d9812dac8eec181a12ed50799eba
Bituvo <bituvocoding@gmail.com>
2023-04-23 08:57:07 -0400

    Fix: Update score calculation

--- nextbots/definitions.lua
+++ nextbots/definitions.lua
@@ -27,7 +27,7 @@ local common_nextbot_definition = {
 		else
 			score = target_chased_time / (target_deaths + 3)
 		end
-		score = score + self.speed / 8
+		score = score + self.speed / 15
 		target_meta:set_float("score", score)
 
 		minetest.chat_send_all(self.target:get_player_name() .. " was killed by " .. self.formal_name)
