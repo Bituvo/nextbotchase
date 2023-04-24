f74d6159e2a96cda51027345c324482634438466
Bituvo <bituvocoding@gmail.com>
2023-04-24 19:20:54 -0400

    Refactor: Add newline after textdomain definition

---
+++ script.py
@@ -0,0 +1,23 @@
+import os
+
+# function to duplicate and modify translation files
+def modify_translation_files(dir_path, target_lang_code):
+    for root, dirs, files in os.walk(dir_path):
+        for filename in files:
+            if filename.endswith(".tr"):
+                textdomain, lang_code, ext = filename.split(".")
+                if lang_code != target_lang_code:
+                    new_filename = f"{textdomain}.{target_lang_code}.tr"
+                    src_path = os.path.join(root, filename)
+                    dst_path = os.path.join(root, new_filename)
+                    with open(src_path, "r", encoding="utf-8") as src_file:
+                        with open(dst_path, "w", encoding="utf-8") as dst_file:
+                            for line in src_file:
+                                if "=" in line:
+                                    key, value = line.split("=")
+                                    dst_file.write(f"{key}={value.strip()}\n")
+                                else:
+                                    dst_file.write(line)
+
+# example usage
+modify_translation_files(os.getcwd(), "fr") # change target language code as desired
