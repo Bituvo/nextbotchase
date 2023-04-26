8c0995dd50e7c5cd7e8f588f958622476513bac5
Bituvo <bituvocoding@gmail.com>
2023-04-26 18:17:37 -0400

    Temporary debug code

--- nextbots/logic.lua
+++ nextbots/logic.lua
@@ -21,7 +21,9 @@ function nextbots.spawn_nextbot(name, pos, target, wait_time)
 			return
 		end
 
-		target:get_meta():set_int("being_chased", 1)
+		local target_meta = target:get_meta()
+		
+		target_meta:set_int("being_chased", 1)
 		new_nextbot:get_luaentity()._chasing = true
 
 		-- Play sound
