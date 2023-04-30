a1e35ef48aaeff19494f5e85a1d1185ba009282a
Bituvo <bituvocoding@gmail.com>
2023-04-30 16:48:08 -0400

    Fix: Remove nextbot when target leaves

--- server/playerregistrations.lua
+++ server/playerregistrations.lua
@@ -60,3 +60,8 @@ minetest.register_on_respawnplayer(function(player)
 	nextbots.handle_new_player(player)
 	return true
 end)
+
+minetest.register_on_leaveplayer(function(player)
+	local nextbot_id = player:get_meta():get_int("nextbot_id")
+	nextbots.remove_nextbot(nextbot_id)
+end)
