9a181c254436a9089477378d1a3fe63720c074cb
Bituvo <bituvocoding@gmail.com>
2023-05-05 22:02:33 -0400

    Feat: Add "/skin" chatcommand to server status info

--- server/chatstatus.lua
+++ server/chatstatus.lua
@@ -2,7 +2,7 @@ local S = minetest.get_translator("server")
 
 function minetest.get_server_status(name)
 	local message = minetest.colorize("#ff0", S("Welcome to BACKROOMS CHASE, ")) .. name
-	message = message .. " | " .. minetest.colorize("#9ef", S('Type "/who" to see who is online'))
+	message = message .. " | " .. minetest.colorize("#9ef", S('Type "/who" to see who is online, "/skin" to change your skin'))
 
 	return message
 end
